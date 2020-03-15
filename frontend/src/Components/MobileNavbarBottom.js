b537a999f10562805ca0890bf9311f812a07a589
urielquevedo <quevedouriel3@gmail.com>
2020-03-14 23:21:03 -0300

    mobile navbar translate

--- frontend/src/Components/MobileNavbarBottom.js
+++ frontend/src/Components/MobileNavbarBottom.js
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import M from "materialize-css";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const MobileNavbarBottom = () => {
   const [sideNav, setSideNav] = useState();
+  const { t } = useTranslation();
 
   useEffect(() => {
     const options = {
-      edge: 'right',
-    }
+      edge: "right"
+    };
 
     const elem = document.querySelectorAll(".sidenav");
     const sidenav = M.Sidenav.init(elem, options);
 
-    const collapsibleElem = document.querySelectorAll('.collapsible');
+    const collapsibleElem = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibleElem);
 
     setSideNav(sidenav[0]);
@@ -23,88 +25,130 @@ const MobileNavbarBottom = () => {
   return (
     <>
       <div className="show-on-med-only hide-on-large-only mobile-navbar-bottom">
-        <nav  className="mobile-nav-bottom">
+        <nav className="mobile-nav-bottom">
           <div>
-            <ul style={{ display:'flex', justifyContent:'space-around' }}>
+            <ul style={{ display: "flex", justifyContent: "space-around" }}>
               <li>
                 <Link to="/">
                   <i class="material-icons mobile-navbar-bottom-icons">home</i>
-                  <p className="mobile-navbar-bottom-item">Home</p>
+                  <p className="mobile-navbar-bottom-item">{t('Home')}</p>
                 </Link>
               </li>
               <li>
                 <i class="material-icons mobile-navbar-bottom-icons">search</i>
-                <p className="mobile-navbar-bottom-item">Search</p>
+                <p className="mobile-navbar-bottom-item">{t('Search')}</p>
               </li>
               <li>
-                <i class="material-icons mobile-navbar-bottom-icons">notifications_none</i>
-                <p className="mobile-navbar-bottom-item">Notify</p>
+                <i class="material-icons mobile-navbar-bottom-icons">
+                  notifications_none
+                </i>
+                <p className="mobile-navbar-bottom-item">{t('Notify')}</p>
               </li>
-              <li data-target="mobile-demo" className="sidenav-trigger" style={{margin:"0px", cursor:'pointer'}}>
+              <li
+                data-target="mobile-demo"
+                className="sidenav-trigger"
+                style={{ margin: "0px", cursor: "pointer" }}
+              >
                 <i class="material-icons collapse-icon">menu</i>
-                <p className="mobile-navbar-bottom-item" style={{ transform:'translate(1.2px, -15px)' }}>More</p>
+                <p
+                  className="mobile-navbar-bottom-item"
+                  style={{ transform: "translate(1.2px, -15px)" }}
+                >
+                  {t('More')}
+                </p>
               </li>
             </ul>
           </div>
         </nav>
       </div>
-      <ul class="sidenav show-on-med-only hide-on-large-only collapse-menu"  id="mobile-demo">
+      <ul
+        class="sidenav show-on-med-only hide-on-large-only collapse-menu"
+        id="mobile-demo"
+      >
         <li className="row">
           <div class="user-view col s12 m6 offset-m3 collapse-user">
             <div class="background">
               <img src="https://image.freepik.com/vector-gratis/banner-concepto-pelicula-cine-estilo-dibujos-animados_98402-1682.jpg" />
             </div>
-            <a href="#user"><img class="circle" src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg" /></a>
-            <a href="#name"><span class="white-text name">Uriel Quevedo</span></a>
-            <a href="#email"><span class="white-text email">quevedouriel3@gmail.com</span></a>
+            <a href="#user">
+              <img
+                class="circle"
+                src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg"
+              />
+            </a>
+            <a href="#name">
+              <span class="white-text name">Uriel Quevedo</span>
+            </a>
+            <a href="#email">
+              <span class="white-text email">quevedouriel3@gmail.com</span>
+            </a>
           </div>
         </li>
         <div className="row">
           <ul className="col s12 m6 offset-m3 collapsible ">
-            <li className="row" style={{ margin:'0px'}}>
-              <i class="material-icons col s2 collapse-menu-icon">account_box</i>
-              <div className="col s10 collapse-menu-font">
-                Profile
-              </div>
+            <li className="row" style={{ margin: "0px" }}>
+              <i class="material-icons col s2 collapse-menu-icon">
+                account_box
+              </i>
+              <div className="col s10 collapse-menu-font">{t('Profile')}</div>
             </li>
-            <li className="row" style={{ margin:'0px'}}>
-              <i class="material-icons col s2 collapse-menu-icon">brightness_4</i>
+            <li className="row" style={{ margin: "0px" }}>
+              <i class="material-icons col s2 collapse-menu-icon">
+                brightness_4
+              </i>
               <div className="switch">
-                <div className="col s6 collapse-menu-font">
-                  Dark Mode
-                </div> 
-                <label className="switch col s4" style={{textAlign:'center'}}>
-                  <input 
+                <div className="col s6 collapse-menu-font">{t('Dark Theme')}</div>
+                <label
+                  className="switch col s4"
+                  style={{ textAlign: "center" }}
+                >
+                  <input
                     //defaultChecked={isDarkThemeActive}
                     type="checkbox"
                     //onClick={() => changeTheme(setTheme)}
                   />
-                  <span class="lever" style={{ margin:'0'}}></span>
+                  <span class="lever" style={{ margin: "0" }}></span>
                 </label>
               </div>
             </li>
             <li>
-              <div class="collapsible-header row" style={{margin:'0px', padding:'0px'}}>
-                <i class="material-icons col s2 collapse-menu-icon" style={{margin:'0px'}}>g_translate</i>
-                <div className="col s10 collapse-menu-font">
-                  Lenguage
-                </div>
+              <div
+                class="collapsible-header row"
+                style={{ margin: "0px", padding: "0px" }}
+              >
+                <i
+                  class="material-icons col s2 collapse-menu-icon"
+                  style={{ margin: "0px" }}
+                >
+                  g_translate
+                </i>
+                <div className="col s10 collapse-menu-font">{t('Lenguage')}</div>
               </div>
-              <div class="collapsible-body collapsible-menu" style={{textAlign:'center'}}><span>Spanish (es-ES)</span></div>
+              <div
+                class="collapsible-body collapsible-menu"
+                style={{ textAlign: "center" }}
+              >
+                <span>Spanish (es-ES)</span>
+              </div>
             </li>
-            <li className="row" style={{ margin:'0px'}}>
-              <i class="material-icons col s2 collapse-menu-icon">exit_to_app</i>
-              <div className="col s10 collapse-menu-font">
-                Log Out
-              </div> 
+            <li className="row" style={{ margin: "0px" }}>
+              <i class="material-icons col s2 collapse-menu-icon">
+                exit_to_app
+              </i>
+              <div className="col s10 collapse-menu-font">{t('Log Out')}</div>
             </li>
           </ul>
         </div>
         <li className="collapse-menu-close">
-          <i className="material-icons collapse-menu-icon-close" onClick={() => sideNav.close()}>highlight_off</i>
+          <i
+            className="material-icons collapse-menu-icon-close"
+            onClick={() => sideNav.close()}
+          >
+            highlight_off
+          </i>
         </li>
-    </ul>
-  </>
+      </ul>
+    </>
   );
 };
 
--- frontend/src/Components/MobileNavbarTop.js
+++ frontend/src/Components/MobileNavbarTop.js
@@ -1,29 +1,71 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const MobileNavbarTop = () => {
-  return ( 
-    <div className="show-on-med-only hide-on-large-only" style={{position:"fixed", top:'-1px', width:'100%', background:'#020923'}}>
+  const { t } = useTranslation();
+
+  return (
+    <div
+      className="show-on-med-only hide-on-large-only"
+      style={{
+        position: "fixed",
+        top: "-1px",
+        width: "100%",
+        background: "#020923"
+      }}
+    >
       <nav className="mobile-nav-top">
         <div class="nav-wrapper">
-          <ul style={{ display:'flex', justifyContent:'space-around', alignItems:'center' }}>
+          <ul
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center"
+            }}
+          >
             <li>
-              <Link  activeClassName='item-selected' className='nav-item' to="/movies">movies</Link>
+              <Link
+                activeClassName="item-selected"
+                className="nav-item"
+                to="/movies"
+              >
+                {t('movies')}
+              </Link>
             </li>
             <li>
-              <Link  activeClassName='item-selected' className='nav-item' to="/programs">programs</Link>
+              <Link
+                activeClassName="item-selected"
+                className="nav-item"
+                to="/programs"
+              >
+               {t('programs')}
+              </Link>
             </li>
             <li>
-              <Link  activeClassName='item-selected' className='nav-item' to="/trailers">trailers</Link>
+              <Link
+                activeClassName="item-selected"
+                className="nav-item"
+                to="/trailers"
+              >
+                {t('trailers')}
+              </Link>
             </li>
             <li>
-              <Link  activeClassName='item-selected' className='nav-item' style={{ marginRight:'10px'}} to="/mylist">mylist</Link>
+              <Link
+                activeClassName="item-selected"
+                className="nav-item"
+                style={{ marginRight: "10px" }}
+                to="/mylist"
+              >
+                {t('mylist')}
+              </Link>
             </li>
           </ul>
         </div>
       </nav>
     </div>
   );
-}
- 
+};
+
 export default MobileNavbarTop;
