7c18834aa065306643aeaecde0b95aa55f2015b2
urielquevedo <quevedouriel3@gmail.com>
2020-03-28 03:19:55 -0300

    basic programs api implemented

--- backend/src/persistence/ProgramDaoFirebase.js
+++ backend/src/persistence/ProgramDaoFirebase.js
@@ -1,16 +1,59 @@
 const admin = require("firebase-admin");
 const db = admin.firestore();
+const collection = db.collection('programs');
 
 const addProgram = (program) => {
-  db.collection('programs').add(program);
+  collection.add(program);
 }
 
-const test = () => {
-  db.collection('test1').doc('3').get()
-    .then(d => d.data().referen.get().then(p => console.log(p.data())))
+const getProgram = (uid) => {
+  return collection.doc(uid)
+    .get()
+    .then(doc => JSON.parse(JSON.stringify(doc.data())));
+}
+
+const getSeason = (uid, season_number) => {
+  return collection.doc(uid)
+    .then((doc) => {
+      const program = JSON.parse(JSON.stringify(doc.data()));
+      return program.seasons[season_number];
+    })
+}
+
+const getEpisode = (uid, season_number, episode_number) => {
+  return collection.doc(uid)
+    .then((doc) => {
+      const program = JSON.parse(JSON.stringify(doc.data()));
+      return program.seasons[season_number].episodes[episode_number];
+    })
+}
+
+const getLatestEpisodes = (lastItem, limit) => {
+  return db.collection('episodes')
+    .orderBy('date', 'asc')
+    .limit(limit)
+    .offset(lastItem)
+    .get()
+    .then((snap) => {
+      const episodes = []
+      snap.forEach((doc) => {
+        const little_episode_inf = doc.data();
+        const season_number = little_episode_inf.season_number;
+        const episode_number = little_episode_inf.episode_number;
+        little_episode_inf.reference.get()
+          .then((program_doc) => {
+            const episode = program_doc.data().seasons[season_number].episodes[episode_number];
+            episodes.push(episode);
+          })
+      })
+      return episodes;
+    })
 }
 
 module.exports = {
   addProgram,
-  test,
+  getProgram,
+  getSeason,
+  getEpisode,
+  getLatestEpisodes,
 };
